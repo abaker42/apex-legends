ec9cc285917861401485e77765ba73a610c04e3a
abaker42 <anthony.baker390@gmail.com>
2022-10-06 03:10:52 -0400

    Inital data pull showing on screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { Fragment, useEffect, useState } from 'react';
 import './App.css';
+import Player from './components/Player';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log('im in app');
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
+	const [transData, setTransData] = useState({
+		userName: '',
+		platform: '',
+		rankName: '',
+		rankDiv: '',
+	});
+
+	useEffect(() => {
+		const fetchPlayer = async () => {
+			setError(null);
+			setIsLoading(true);
+			try {
+				const resp = await fetch(
+					'https://api.mozambiquehe.re/bridge?auth=142181252df5d8e977810798153249ee&player=icecold_phenom&platform=PS4'
+				);
+				if (!resp.ok) {
+					console.log('im looking down');
+					throw new Error('Username/Platform invalid!');
+				}
+				const data = await resp.json();
+				console.log(data);
+				// const transformData = data.results.map((stats) => {
+				// 	return {
+				// 		userName: stats.global.name,
+				// 		platform: stats.global.platform,
+				// 		rankName: data.global.rank.rankName,
+				// 		rankDiv: data.global.rank.rankDiv,
+				// 		//lastLegend: legends.selected.LegendName,
+				// 	};
+				//});
+
+				setTransData({
+					userName: data.global.name,
+					platform: data.global.platform,
+					rankName: data.global.rank.rankName,
+					rankDiv: data.global.rank.rankDiv,
+				});
+
+				console.log(transData);
+				const playerData = [];
+				for (const key in data) {
+					playerData.push();
+				}
+			} catch (error) {
+				setError(error.message);
+			}
+			setIsLoading(false);
+		};
+
+		fetchPlayer();
+	}, []);
+
+	return (
+		<Fragment>
+			<h2>Welcome to Apex</h2>
+			<Player
+				userName={transData.userName}
+				platform={transData.platform}
+				rankName={transData.rankName}
+				rankDiv={transData.rankDiv}
+			/>
+		</Fragment>
+	);
 }
 
 export default App;
